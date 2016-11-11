a947fc98a9d2078c5a2ce596fcaf8f76e75010cc
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-11-11 19:05:39 +0200

    Saving changes

--- docker/ui/ubuntu-trusty/simple/run.sh
+++ docker/ui/ubuntu-trusty/simple/run.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" == "$1"; }
-
-docker_version=$(docker version | grep 'Client version' | awk '{split($0,a,":"); print a[2]}' | tr -d ' ')
-
-# Docker 1.3.0 or later is required for --device
-if ! version_gt "${docker_version}" "1.2.0"; then
-	echo "Docker version 1.3.0 or greater is required"
-	exit 1
-fi
-
 USER_UID=$(id -u)
 USER_GID=$(id -g)
 XSOCK=/tmp/.X11-unix
@@ -26,5 +16,5 @@ docker run \
 	--env="USER_UID=${USER_UID}" \
 	--env="USER_GID=${USER_GID}" \
 	--env="DISPLAY=${DISPLAY}" \
-	-u dox \
+	-u dev \
 	"$@"
